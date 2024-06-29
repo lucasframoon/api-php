a6a42c275f666504d762f1ce38910032bf0a4e41
Lucas Franco <lucasframoon@gmail.com>
2024-06-28 22:16:55 -0300

    api-4: Add User model

---
+++ src/Model/User.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Src\Model;
+
+use Src\Model\Model;
+
+class User extends Model
+{
+    public function __construct(
+        private int $id = 0,
+        private string $name = '',
+        private string $email = '',
+        private string $password = ''
+    ) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+}
